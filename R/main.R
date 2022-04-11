a4023d8a1b0f295a670adcc5d4668d2a8d97ec92
RavenGan <dailin.raven.gan@gmail.com>
2022-04-11 10:11:42 -0400

    Finish local package checking

--- R/main.R
+++ R/main.R
@@ -10,10 +10,27 @@
 # 6. combine: TURE returns both raw and integrated batches with all batched combined.
 #    FALSE returns a list which contains all the integrated batches. The default is TRUE.
 
+globalVariables(c("cluster_assignment" # used in the newly created metadata
+                  ))
 
-
+#' Batch effect removal with SCIBER
+#'
+#' @param batches_clean A list contains all the pre-processed matrices with dimension of n_genes*n_cells.
+#' @param ref_index The index of the reference batch in the object "batches_clean"
+#' @param batches_meta_data A list contains the meta data for all the batches. The order should be consistent with that in "batches_clean"
+#' @param top_pairs_with_ref Proportion of matched clusters. Default uses the 0.05 significance level.
+#' @param top_genes The number of marker genes used for Fisher exact test.
+#' @param n_core Specify the number of cores otherwise use all the available cores.
+#' @param combine TURE returns both raw and integrated batches with all batched combined. FALSE returns a list which contains all the integrated batches. The default is TRUE.
+#'
+#' @return A list of both raw and integrated batches with all batched combined or a list which contains all the integrated batches.
+#' @importFrom dplyr %>%
+#' @export
+#'
+# @examples
+#SCIBER_int(batches_clean, ref_index, batches_meta_data, top_pairs_with_ref, n_core = 8, combine = TRUE)
 SCIBER_int <- function(batches_clean, ref_index,
-                       batches_meta_data, top_pairs_with_ref,
+                       batches_meta_data, top_pairs_with_ref, top_genes = 50,
                        n_core = parallel::detectCores(), combine = TRUE
                        ) {
   datasets_cluster <- obtain_clustered_data(ref_index, batches_clean, numCores = n_core)
